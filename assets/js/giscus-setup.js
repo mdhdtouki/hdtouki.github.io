2b041578bad4fbb79513359696174f940763ce86
mdhdtouki <142306566+mdhdtouki@users.noreply.github.com>
2026-01-19 06:46:56 +0000

    Deploying to gh-pages from @ mdhdtouki/mdhdtouki.github.io@e332fb7fee8b79f82cd2b1ea53e1502921cfa741 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "mdhdtouki/hdtouki.github.io",
+    "data-repo": "mdhdtouki/mdhdtouki.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",

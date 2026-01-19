d0d97edf63193c2269969efcecbd3a4967d25fc5
mdhdtouki <142306566+mdhdtouki@users.noreply.github.com>
2026-01-19 16:06:34 +0000

    Deploying to gh-pages from @ mdhdtouki/mdhdtouki.github.io@ba56ae19a3f80d2404898f7f40fd23801988377d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "Projects",
-          description: "A growing collection of your cool projects.",
+          description: "A collection of my academic projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";

89c2a8b6947b0b41b525aad62eb5077e569a4685
mdhdtouki <142306566+mdhdtouki@users.noreply.github.com>
2026-01-20 00:25:50 +0000

    Deploying to gh-pages from @ mdhdtouki/mdhdtouki.github.io@4090ba8128cce53eac4ea201dd23ea0b8f1bac6a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -411,11 +411,6 @@ ninja.data = [{
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
